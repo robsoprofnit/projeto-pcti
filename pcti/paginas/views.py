cd40f185196becfc160dd7c36ff6795bdb8d339e
robsoprofnit <robsonunifap@gmail.com>
2022-04-06 20:19:56 -0300

    excluir modelo2.html

--- pcti/paginas/views.py
+++ pcti/paginas/views.py
@@ -6,18 +6,10 @@ class PaginaInicial(TemplateView):
     template_name = 'index.html'
 
 
-# class RelatoriosView(TemplateView):
-#    template_name = ''
-
-
 class SobreView(TemplateView):
     template_name = 'sobre.html'
 
 
 class DashboardView(TemplateView):
     template_name = 'dashboard.html'
-
-
-class PaginaInicial2(TemplateView):
-    template_name = 'modelo2.html'
 
