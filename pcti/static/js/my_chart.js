79f7d84ff7a25dd87929ef282105fdde43b7b04c
robsoprofnit <robsonunifap@gmail.com>
2022-07-01 17:54:00 -0300

    atualizando layout dashboard.html graficos.

--- pcti/static/js/my_chart.js
+++ pcti/static/js/my_chart.js
@@ -1,117 +1,191 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var earning = document.getElementById('earning').getContext('2d');
-var investimentos = document.getElementById('investimentos').getContext('2d');
-var dimensoes = document.getElementById('dimensoes').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-        labels: ['Humanas', 'Exatas', 'Saúde'],
-        datasets: [{
-            label: '# of Votes',
-            data: [50, 35, 80],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-    }
-});
-
-var myChart = new Chart(earning, {
-    type: 'bar',
-    data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-        datasets: [{
-            label: 'Patentes em 2021',
-            data: [1256, 5919, 6543, 2995, 4612, 5483, 5512, 4419, 4183, 4745, 3282, 2383],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-    }
-});
-
-var myChart = new Chart(dimensoes, {
-    type: 'radar',
-    data : {
-        labels: [
-            'Recursos Aplicados',
-            'Recursos Humanos',
-            'Bolsas de Formação',
-            'Produção Ciêntífica',
-            'Patentes',
-            'Inovação'
-        ],
-        datasets: [{
-            label: 'Primeiro Semestre 2021',
-            data: [65, 59, 90, 81, 56, 55],
-            fill: true,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgb(255, 99, 132)',
-            pointBackgroundColor: 'rgb(255, 99, 132)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(255, 99, 132)'
-        }, {
-            label: 'Segundo Semestre 2021',
-            data: [28, 48, 40, 19, 96, 27],
-            fill: true,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgb(54, 162, 235)',
-            pointBackgroundColor: 'rgb(54, 162, 235)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(54, 162, 235)'
-        }]
-    },
-    options: {
-        responsive: true,
-    }
-});
+var recaplic = document.getElementById('recaplic').getContext('2d');
+var rechuman = document.getElementById('rechuman').getContext('2d');
+var bolforma = document.getElementById('bolforma').getContext('2d');
+var prodcien = document.getElementById('prodcien').getContext('2d');
+var patentes = document.getElementById('patentes').getContext('2d');
+var inovacao = document.getElementById('inovacao').getContext('2d');
 
 
-var myChart = new Chart(investimentos, {
+var myChart = new Chart(recaplic, {
     type: 'line',
     data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
         datasets: [{
-            label: 'Investimentos',
-            data: [1256, 5919, 6543, 2995, 4612, 5483, 5512, 4419, 4183, 4745, 3282, 2383],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-        }]
+            data: [0, 20, 50],
+            label: 'Ciência e Tecnologia',
+            borderColor: ['blue'],
+            backgroundColor: ['blue'],
+        }, {
+            data: [0, 12, 45],
+            label: 'Pesquisa e Desenvolvimento',
+            borderColor: ['orange'],
+            backgroundColor: ['orange'],
+        }, {
+            data: [0, 37, 10],
+            label: 'Atividades Científicas e Técnicas Correlata',
+            borderColor: ['green'],
+            backgroundColor: ['green'],
+        }],
+        labels: ['2019','2020', '2021']
     },
     options: {
-        responsive: true,
+
     }
+});
+
+var myChart = new Chart(rechuman, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            data: [0, 20, 50],
+            label: 'Privado sem fins lucrativos',
+            borderColor: ['blue'],
+            backgroundColor: ['blue'],
+        }, {
+            data: [0, 12, 45],
+            label: 'Governo',
+            borderColor: ['yellow'],
+            backgroundColor: ['yellow'],
+        }, {
+            data: [0, 37, 10],
+            label: 'Empresarial',
+            borderColor: ['green'],
+            backgroundColor: ['green'],
+        },  {
+            data: [0, 23, 40],
+            label: 'Ensino superior',
+            borderColor: ['purple'],
+            backgroundColor: ['purple'],
+        },  {
+            data: [0, 92, 145],
+            label: 'Total',
+            borderColor: ['brown'],
+            backgroundColor: ['brown'],
+        }],
+        labels: ['2019','2020', '2021']
+    },
+    options: {
+
+    },
+});
+
+var myChart = new Chart(bolforma, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [0, 20, 50],
+            label: 'no País',
+            borderColor: ['blue'],
+            backgroundColor: ['blue'],
+        }, {
+            data: [0, 12, 45],
+            label: 'no Exterior',
+            borderColor: ['orange'],
+            backgroundColor: ['orange'],
+        }, {
+            data: [0, 32, 95],
+            label: 'Total',
+            borderColor: ['green'],
+            backgroundColor: ['green'],
+        }],
+        labels: ['2019','2020', '2021']
+    },
+    options: {
+
+    }
+});
+
+
+var mixedChart = new Chart(prodcien, {
+    data: {
+        datasets: [{
+
+            type: 'line',
+            data: [0, 2.76, 2.32],
+            label: '% em relação ao Mundo',
+            borderColor: ['blue'],
+            backgroundColor: ['blue'],
+            yAxisID: 'right-y-axis'
+
+
+        }, {
+            type: 'bar',
+            data: [0, 89241, 90000],
+            label: 'Nº de artigos',
+            borderColor: ['yellow'],
+            backgroundColor: ['yellow'],
+            yAxisID: 'left-y-axis'
+        }],
+        labels: ['2019','2020', '2021']
+    },
+    options: {
+        scales: {
+            'left-y-axis': {
+                type: 'linear',
+                position: 'left'
+            },
+            'right-y-axis': {
+                type: 'linear',
+                position: 'right'
+            }
+        }
+    }
+});
+
+
+var myChart = new Chart(patentes, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [0, 45, 56],
+            label: 'Total',
+            borderColor: ['green'],
+            backgroundColor: ['green'],
+        }, {
+            data: [0, 35, 44],
+            label: 'Patente de Invenção',
+            borderColor: ['yellow'],
+            backgroundColor: ['yellow'],
+        }, {
+            data: [0, 10, 12],
+            label: 'Modelo de Utilidade',
+            borderColor: ['cyan'],
+            backgroundColor: ['cyan'],
+        }],
+        labels: ['2019','2020', '2021']
+    },
+    options: {
+
+    }
+});
+
+
+var myChart = new Chart(inovacao, {
+    type: 'bar',
+    data: {
+        datasets: [{
+            data: [0, 20, 50],
+            label: 'Total',
+            borderColor: ['blue'],
+            backgroundColor: ['blue'],
+        }, {
+            data: [0, 12, 45],
+            label: 'Industria',
+            borderColor: ['yellow'],
+            backgroundColor: ['yellow'],
+        }, {
+            data: [0, 37, 10],
+            label: 'Serviços',
+            borderColor: ['green'],
+            backgroundColor: ['green'],
+        },  {
+            data: [0, 23, 40],
+            label: 'Eletricidade e Gás',
+            borderColor: ['purple'],
+            backgroundColor: ['purple'],
+        }],
+        labels: ['2019','2020', '2021']
+    },
+    options: {
+
+    },
 });
