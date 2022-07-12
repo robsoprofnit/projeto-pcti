a0cf3ad733e22c13e862fa9996a82b3e4729a66d
robsoprofnit <robsonunifap@gmail.com>
2022-07-12 20:21:53 -0300

    Campos dinâmicos Dashboar Ano e Instituição

--- pcti/paginas/urls.py
+++ pcti/paginas/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', InitialView.as_view(), name='inicio'),
     path('manager/', ManagerView.as_view(), name='manager'),
     path('sobre/', SobreView.as_view(), name='sobre'),
-    path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('dashboard/<int:year>/<str:name>/', DashboardView.as_view(), name='dashboard'),
     path('configuracoes/', ConfiguracoesView.as_view(), name='configuracoes'),
     path('modeloadm/', ModeloAdmView.as_view(), name='modelo-adm'),
 ]
